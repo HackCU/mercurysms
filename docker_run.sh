2075655e21f661080c74614cea7960da73c757b3
Gerard Casas Saez <casassg@gmail.com>
2017-04-14 15:49:33 -0600

    update docker image tag

--- docker_run.sh
+++ docker_run.sh
@@ -3,4 +3,4 @@
 
 # Make sure to create env.list before, use env.list.template as a template
 
-docker run -p 8000:80 --env-file ./env.list -v $(pwd)/db.sqlite3:/code/db.sqlite3 hackcu/mercurysms:1.0.0
+docker run -p 8000:80 --env-file ./env.list -v $(pwd)/db.sqlite3:/code/db.sqlite3 hackcu/mercurysms:1.0.1
