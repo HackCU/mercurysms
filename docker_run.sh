0af48668f339be96ed2eecb73d27cfaac2e487cf
Gerard Casas Saez <casassg@gmail.com>
2017-04-14 15:57:01 -0600

    run docker detached mode

--- docker_run.sh
+++ docker_run.sh
@@ -3,4 +3,4 @@
 
 # Make sure to create env.list before, use env.list.template as a template
 
-docker run -p 8000:80 --env-file ./env.list -v $(pwd)/db.sqlite3:/code/db.sqlite3 hackcu/mercurysms:1.0.1
+docker run -d -p 8000:80 --env-file ./env.list -v $(pwd)/db.sqlite3:/code/db.sqlite3 hackcu/mercurysms:1.0.1
